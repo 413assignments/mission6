4b86957347c00dcb0e08ea3c84a3d63bb1c28d5c
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-02-16 22:29:56 -0700

    commit

--- MIssion06_Bassett/MIssion06_Bassett/obj/Debug/net8.0/MIssion06_Bassett.AssemblyInfo.cs
+++ MIssion06_Bassett/MIssion06_Bassett/obj/Debug/net8.0/MIssion06_Bassett.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MIssion06_Bassett")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af19a3b3ebfdbe6389758972fc21d32d99917e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MIssion06_Bassett")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MIssion06_Bassett")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
